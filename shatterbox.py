50974b2215a833ace0c8824d1049c1e270853463
Edgecase963 <edgecase963@gmail.com>
2020-09-15 14:29:44 -0400

    Much Better Physics
    Now uses Pymunk

--- shatterbox.py
+++ shatterbox.py
@@ -6,6 +6,8 @@ import sys
 import math
 import random
 import numpy as np
+import pymunk
+from pymunk import Vec2d
 
 
 
@@ -105,7 +107,8 @@ def spriteDistance(sprite1, sprite2):
 
 
 class Sprite(QtWidgets.QGraphicsPixmapItem):
-    def __init__(self, pos, width, height, image=None, parent=None, collisionInt=.5):
+    def __init__(self, pos, width, height, environment,
+                 mass=10, friction=.3, elasticity=.5, image=None, parent=None, collisionInt=.5):
         # "pos" should be a QtCore.QPointF class
 
         self.parent = parent   # Look at self.setParentItem(<item>)
@@ -115,9 +118,14 @@ class Sprite(QtWidgets.QGraphicsPixmapItem):
         # Structure: {<sprite>: <angle>}
         # Angle is in degrees
 
+        self.environment = environment
+
         if image:
             self.pixmap = QtGui.QPixmap(image)
             self.setPixmap( self.pixmap.scaled(width, height, QtCore.Qt.KeepAspectRatio) )
+
+        self.setTransformOriginPoint(width/2, height/2)
+
         #self.setFlag(QtGui.QGraphicsPixmapItem.ItemIsSelectable)
         #self.setFlag(QtGui.QGraphicsPixmapItem.ItemIsMovable)
         self.setAcceptHoverEvents(True)
@@ -129,17 +137,31 @@ class Sprite(QtWidgets.QGraphicsPixmapItem):
 
         self.limitedBoundary = True   # If set to True, this sprite will bounce off the edges of the scene
 
+        # Pymunk
+        #--
+
+        self.radius = width/2
+
+        inertia = pymunk.moment_for_circle(mass, 0, self.radius, (0,0))
+
+        self.body = pymunk.Body(mass, inertia)
+        self.body.position = pos
+
+        self.shape = pymunk.Circle(self.body, self.radius, Vec2d(0,0))
+        self.shape.friction = friction
+        self.shape.elasticity = elasticity
+        self.shape.sprite = self
+
+        self.connections = {}
+        # Structure: {<Sprite>: <pymunk.PinJoint>}
+
+        #--
+
         #self.setPos(self.xPos, self.yPos)
         self.setPos(pos[0], pos[1])
 
         self.lastUpdated = time.time()
 
-        self.rotation = 0.
-        self.rotate_speed = 0.
-        self.vel = [0., 0.]
-        self.friction = 8.   # The percentage of movement speed to subtract per second
-        # If set to 0, the sprite will not slow down
-        self.frictionCutOff = 0.01   # If the movement speed falls below this, it will be set to 0.0 just to help keep things simple
         self.elasticity = 1.
 
         self.mouseHoverFunc = None   # Executes with (self, event)
@@ -160,12 +182,6 @@ class Sprite(QtWidgets.QGraphicsPixmapItem):
         tempPixmap = self.pixmap.scaled(width, height, QtCore.Qt.KeepAspectRatio)
         self.setPixmap( tempPixmap )
 
-    def getPos(self):
-        return self.pos()
-
-    def getCollisions(self):
-        return self.collidingItems()
-
     def getCenter(self):
         childrenList = self.childItems()
         if not childrenList:
@@ -182,157 +198,142 @@ class Sprite(QtWidgets.QGraphicsPixmapItem):
 
     def bump(self, direction, speed, invert=False):
         # Set "invert" to True if you want the sprite to move away from the target
-        self.vel = getDirection(self.getCenter().x(), self.getCenter().y(), direction.x(), direction.y())
-        self.vel = [i*speed for i in self.vel]
+        newVel = getDirection(self.body.position[0], self.body.position[1], direction[0], direction[1])
+        newVel = [i*speed for i in newVel]
+        self.body.velocity = Vec2d(newVel)
 
     def collision(self, item):
         pass
 
-    def collide(self, item):
-        self.collisions[item] = time.time()
-        item.collisions[self] = time.time()
+    def connectTo(self, sprite):
+        # angle is in degrees
+        direction = getDirection(self.getCenter().x(), self.getCenter().y(), sprite.getCenter().x(), sprite.getCenter().y())
 
-        self.collision(item)
-        item.collision(self)
+        point1 = (direction[0]*self.radius, direction[1]*self.radius)
+        point2 = (-direction[0]*self.radius, -direction[1]*self.radius)
+        c = pymunk.PinJoint(self.body, sprite.body, point1, point2)
+        self.body.space.add(c)
+        self.connections[sprite] = c
+        sprite.connections[self] = c
 
-    def bounceOff(self, sprite):
-        # calculate normal and tangential unit vectors
-        norm_vect = [(sprite.getCenter().x() - self.getCenter().x()),
-                     (sprite.getCenter().y() - self.getCenter().y())]  # stil un-normalized!
-        norm_length = math.sqrt((norm_vect[0]**2) + (norm_vect[1]**2))
-        norm_vect = [norm_vect[0] / norm_length,
-                     norm_vect[1] / norm_length]  # do normalization
-        tang_vect = [-norm_vect[1], norm_vect[0]]  # rotate norm_vect by 90 degrees
-
-        # normal and tangential velocities before collision
-        vel1 = self.vel
-        vel2 = sprite.vel
-        vel1_norm = vel1[0] * norm_vect[0] + vel1[1] * norm_vect[1]
-        vel1_tang = vel1[0] * tang_vect[0] + vel1[1] * tang_vect[1]
-        vel2_norm = vel2[0] * norm_vect[0] + vel2[1] * norm_vect[1]
-        vel2_tang = vel2[0] * tang_vect[0] + vel2[1] * tang_vect[1]
-
-        # calculate velocities after collision
-        new_vel1_norm = (vel1_norm * (self.getWidth() - sprite.getWidth())
-                         + 2 * sprite.getWidth() * vel2_norm) / (self.getWidth() + sprite.getWidth())
-        new_vel2_norm = (vel2_norm * (sprite.getWidth() - self.getWidth())
-                         + 2 * self.getWidth() * vel1_norm) / (self.getWidth() + sprite.getWidth())
-        # no need to calculate new_vel_tang, since it does not change
-
-        # Now update the object's velocity
-        self.vel = [norm_vect[0] * new_vel1_norm + tang_vect[0] * vel1_tang,
-                       norm_vect[1] * new_vel1_norm + tang_vect[1] * vel1_tang]
-        sprite.vel = [norm_vect[0] * new_vel2_norm + tang_vect[0] * vel2_tang,
-                       norm_vect[1] * new_vel2_norm + tang_vect[1] * vel2_tang]
-
-    def updateSprite(self, spriteList, speed=1.0):
+    def updateSprite(self, spriteList, speed):
         uDiff = time.time() - self.lastUpdated
         uDiff = uDiff * speed
 
-        self.vel[0] -= perc2num(self.friction, self.vel[0]*speed)
-        self.vel[1] -= perc2num(self.friction, self.vel[1]*speed)
-        self.rotate_speed -= perc2num(self.friction, self.rotate_speed*speed)
+        self.setPos(self.body.position[0]-self.radius, self.body.position[1]-self.radius)
 
-        if self.limitedBoundary:
-            if self.getRect().left() < 0:   # Too far left
-                self.xPos = 0
-                #if self.movDirection[0] < 0: self.movDirection[0] = -self.movDirection[0]
-                if self.vel[0] < 0:
-                    self.vel[0] = -self.vel[0]
-            if self.getRect().right() > self.scene().sceneRect().width():   # Too far right
-                self.xPos = self.scene().sceneRect().width() - self.getRect().width()
-                #if self.movDirection[0] > 0: self.movDirection[0] = -self.movDirection[0]
-                if self.vel[0] > 0:
-                    self.vel[0] = -self.vel[0]
-            if self.getRect().top() < 0:   # Too far up
-                self.yPos = 0
-                #if self.movDirection[1] < 0: self.movDirection[1] = -self.movDirection[1]
-                if self.vel[1] < 0:
-                    self.vel[1] = -self.vel[1]
-            if self.getRect().bottom() > self.scene().sceneRect().height():   # Too far down
-                self.yPos = self.scene().sceneRect().height() - self.getRect().height()
-                #if self.movDirection[1] > 0: self.movDirection[1] = -self.movDirection[1]
-                if self.vel[1] > 0:
-                    self.vel[1] = -self.vel[1]
+        frictionCut = self.environment.friction * uDiff
+        newVel = list(self.body.velocity)
+        newVel = [i-(i*frictionCut) for i in newVel]
 
-        xPos = self.x()   # Used to set the new x position
-        yPos = self.y()   # Used to set the new y position
+        self.body.velocity = Vec2d(newVel)
 
-        for sprite in [s for s in spriteList if s != self]:
-            distance = spriteDistance(self, sprite)
+        self.body.angular_velocity -= self.body.angular_velocity * frictionCut
 
-            minDistance = 2
-            if sprite in self.connections:
-                minDistance = 3
+        angle = math.degrees(self.body.angle)
 
-            if distance < self.getWidth()/minDistance + sprite.getWidth()/minDistance:
-                if self.parent != sprite and sprite.parent != self:
-                    if self.parent:
-                        self.parent.bounceOff(sprite)
-                    else:
-                        self.bounceOff(sprite)
-
-                jump_distance = self.getWidth()/minDistance + sprite.getWidth()/minDistance
-                jump_distance -= distance
-
-                conditioned_velocity = condition(self.vel)
-
-                jump_x = conditioned_velocity[0] * jump_distance
-                jump_y = conditioned_velocity[1] * jump_distance
-
-                xPos += jump_x/2
-                yPos += jump_y/2
-                sprite.setX(sprite.x() - jump_x/2)
-                sprite.setX(sprite.x() - jump_y/2)
-
-                for item in self.collisions.copy():
-                    if time.time() - self.collisions[item] >= self.collisionInt:
-                        self.collisions.pop(item)
-
-                if not sprite in self.connections:
-                    if not sprite in self.collisions:
-                        self.collide(sprite)
-
-        self.rotation += self.rotate_speed * uDiff
-
-        xPos += self.vel[0] * uDiff
-        yPos += self.vel[1] * uDiff
-
-        self.setPos(xPos, yPos)
+        self.setRotation(angle)
 
         self.update()
-
         self.lastUpdated = time.time()
 
 
 
 class Environment():
-    def __init__(self, scene):
+    def __init__(self, worldView, scene, width, height, friction=20., gravity=0):
+        self.worldView = worldView
         self.scene = scene
+        self.friction = friction   # The percentage of movement speed to subtract per second
+        # If set to 0, the sprite will not slow down
         self.sprites = []
         # A list containing all sprites in this environment
+        self.space = pymunk.Space()
+        self.space.gravity = 0, 0
 
-    def update(self, speed=1.0):
+        self.worldSpeed = .02
+        self.updateSpeed = 50
+
+        ch = self.space.add_collision_handler(0, 0)
+        ch.post_solve = self.collision
+
+        static_body = self.space.static_body
+        static_lines = [pymunk.Segment(static_body, (0, 0), (0, height), 0.0),
+                        pymunk.Segment(static_body, (0, height), (width, height), 0.0),
+                        pymunk.Segment(static_body, (width, height), (width, 0), 0.0),
+                        pymunk.Segment(static_body, (width, 0), (0, 0), 0.0)]
+        for line in static_lines:
+            line.elasticity = 0.95
+            line.friction = 0.9
+        self.space.add(static_lines)
+
+        # Setup timer
+        #--
+        self.worldView.timer = QtCore.QBasicTimer()
+
+        self.worldView.timerEvent = self.update
+        #self.mouseReleaseEvent = self.worldMouseReleaseEvent
+        self.scene.mouseReleaseEvent = self.worldMouseReleaseEvent
+
+        self.worldView.timer.start(self.updateSpeed, self.worldView)
+        #--
+
+        self.scene.keyPressEvent = self.keyPressEvent
+
+    def keyPressEvent(self, e):
+        if e.key() == QtCore.Qt.Key_A:
+            print(1)
+
+    def collision(self, arbiter, space, data):
+        shape1, shape2 = arbiter._get_shapes()
+        if not isinstance(shape1, pymunk.Segment) and not isinstance(shape2, pymunk.Segment):
+            sprite1 = shape1.sprite
+            sprite2 = shape2.sprite
+
+            sprite1.collision(sprite2)
+            sprite2.collision(sprite1)
+
+    def worldMouseReleaseEvent(self, event):
+        pos = event.lastScenePos()
+
+        print("Position: {}, {}".format(pos.x(), pos.y()))
+
+        self.sprites[0].bump([pos.x(), pos.y()], 600)
+
+    def update(self, event):
+        self.space.step(self.worldSpeed)
         for sprite in self.sprites:
-            sprite.updateSprite(self.sprites, speed=speed)
+            sprite.updateSprite(self.sprites, self.worldSpeed)
         self.scene.update( self.scene.sceneRect() )
 
-    def addSprite(self, xy, width, height, image=None, parent=None):
-        newSprite = Sprite(xy, width, height, image=image, parent=parent)
+    def addSprite(self, xy, width, height, mass=10, friction=.3, elasticity=.5, image=None, parent=None):
+        newSprite = Sprite(xy, width, height, self, mass=mass, friction=friction, elasticity=elasticity, image=image, parent=parent)
         self.scene.addItem(newSprite)
         self.sprites.append(newSprite)
+        self.space.add(newSprite.body, newSprite.shape)
         return newSprite
+
+
+
+def setupEnvironment(worldView, scene, friction=20., gravity=0):
+    # worldView should be a `QGraphicsView` item
+    # scene should be a `QGraphicsScene` item
+    sceneRect = scene.sceneRect()
+    environment = Environment(worldView, scene, sceneRect.width(), sceneRect.height(), friction=friction, gravity=gravity)
+    return environment
 
 
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName(_fromUtf8("MainWindow"))
+        self.width = 1050
+        self.height = 1200
         MainWindow.resize(1100, 650)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
 
         self.worldView = QtWidgets.QGraphicsView(self.centralwidget)
+
         self.worldView.setGeometry(QtCore.QRect(15, 20, 1070, 600))
         self.worldView.setObjectName(_fromUtf8("worldView"))
 
@@ -340,38 +341,13 @@ class Ui_MainWindow(object):
         self.timeline.setFrameRange(0, 100)
 
         self.scene = QtWidgets.QGraphicsScene(self.worldView)
+
         self.scene.setSceneRect(0, 0, 1050, 1200)
         self.worldView.setScene(self.scene)
 
         self.worldView.setBackgroundBrush( QtGui.QBrush( QtGui.QColor(180,180,255) ) )
 
-        self.worldView.timerEvent = self.worldTimerEvent
-
         MainWindow.setCentralWidget(self.centralwidget)
-
-    def setupEnvironment(self):
-        self.environment = Environment(self.scene)
-
-        self.worldView.timer = QtCore.QBasicTimer()
-        self.worldView.updateSpeed = 50
-
-        self.worldView.timerEvent = self.worldTimerEvent
-        #self.worldView.mouseReleaseEvent = self.worldMouseReleaseEvent
-        self.scene.mouseReleaseEvent = self.worldMouseReleaseEvent
-
-        self.worldView.timer.start(self.worldView.updateSpeed, self.worldView)
-
-    def worldMouseReleaseEvent(self, event):
-        pos = event.lastScenePos()
-
-        print("Position: {}, {}".format(pos.x(), pos.y()))
-
-        self.environment.sprites[0].bump(pos, 500)
-        self.environment.sprites[0].rotate_speed = 50
-
-    def worldTimerEvent(self, event):
-        self.environment.update(speed=1)
-
 
 
 
@@ -381,17 +357,25 @@ if __name__ == "__main__":
         myapp = Ui_MainWindow()
 
         myapp.setupUi(window)
-        myapp.setupEnvironment()
+        #myapp.setupEnvironment()
+        env = setupEnvironment(myapp.worldView, myapp.scene)
 
-        sprite1 = myapp.environment.addSprite([300,300], 40, 40, image="dot.png")
-        sprite2 = myapp.environment.addSprite([320,320], 40, 40, image="dot.png")
-        #sprite3 = myapp.environment.addSprite([480,480], 20, 20, image="dot.png")
-        #sprite4 = myapp.environment.addSprite([400,460], 20, 20, image="dot.png")
+        sprite1 = env.addSprite([300,300], 40, 40, image="dot.png")
+        sprite2 = env.addSprite([320,320], 40, 40, image="dot.png")
 
-        for i in range(42):
+        sprite1.ff = True
+        sprite2.ff = False
+
+        sprite1.connectTo(sprite2)
+
+        #sprite3 = env.addSprite([480,480], 20, 20, image="dot.png")
+        #sprite4 = env.addSprite([400,460], 20, 20, image="dot.png")
+
+        for i in range(80):
             size = random.randrange(10, 50)
             pos = [random.randrange(50, 1000), random.randrange(50, 1000)]
-            myapp.environment.addSprite(pos, size, size, image="dot.png")
+            sprite = env.addSprite(pos, size, size, image="dot.png")
+            sprite.ff = False
 
         window.show()
         sys.exit(app.exec_())
